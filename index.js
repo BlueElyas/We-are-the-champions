d4923e787b37ec9923ce0731d7775229283877e2
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-09-12 20:34:12 +0100

    Initial commit initial design with JS

---
+++ index.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app"
+import { getDatabase, ref, push, onValue, remove } from 
+"https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+let realtimeDatabase = {
+    databseURL: "https://we-are-the-champions-54d9e-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(realtimeDatabase)
+const database = getDatabase(app) 
+const endorsementsInDB = ref(database, "endorsements")
+const endorsmentInput = document.getElementById("endorsement-element")
+const endorsementBtn = document.getElementById("push-for-endorsement")
+let endorsementListEl = document.getElementById("endorsement-list")
+
+
+endorsementBtn.addEventListener("click", function() {
+    let inputValue = endorsmentInput.value 
+    push(endorsementsInDB, inputValue)
+    clearInputValue()
+
+})
+
+function clearInputValue() {
+    endorsmentInput.innerHTML = ""
+}
+
+onValue(endorsementsInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Objects.entries(snapshot.val())
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            appendItemsToEndorsements(currentItem)
+        }
+        }else {
+            endorsementListEl.innerHTML = "No reviews ...yet"
+    }
+} )
+
+function appendItemsToEndorsements(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newList = document.createElement("li")
+    newList.textContent = itemValue
+    endorsementList.append(newList)
+}
+
+// if (valueInput) {
+//     let newList = document.createElement("li")
+//     newList.textContent = valueInput
+//     endorsementList.append(newList)
+// }
