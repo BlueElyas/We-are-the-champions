e962159a613b92c7d7ca8928441a6d950eeb60ef
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-09-13 03:27:22 +0100

    removed 2 functions from database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js"
-import { getDatabase, ref, push, onValue, child, get } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"
+import { getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"
 
 let realtimeDatabase = {
     databaseURL: "https://we-are-the-champions-54d9e-default-rtdb.europe-west1.firebasedatabase.app/"
